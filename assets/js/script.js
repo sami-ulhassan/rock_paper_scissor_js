9ffbd0b7a065a6a7368f764e29912433fcc96792
Sami Ul Hassan <samihassan0802@gmail.com>
2022-12-09 15:46:26 +0100

    UI creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,120 +2,199 @@ const gameValues = ['rock', 'paper', 'scissors'];
 const totalRounds = 5;
 let roundCount = 1;
 
+const gameResults = {
+    playerWinCount: 0,
+    computerWinCount: 0,
+    displayText: ""
+};
+
+const btnEls = document.querySelectorAll(".btn-primary");
+const playBtnEl = document.querySelector("#btnPlay");
+const btnContainerEl = document.querySelector(".btn-container");
+const playAreaEl = document.querySelector(".play-area");
+const optionsEls = document.querySelectorAll(".option-primary");
+const controlsEl = document.querySelector(".controls-container");
+const roundResultEl = document.querySelector(".round-result");
+const finalResultsEl = document.querySelector(".final-results");
+
+let showPlayOptions = () => {
+    if (roundCount == 1) {
+        btnContainerEl.classList.toggle("d-none")
+        playAreaEl.classList.toggle("d-none");
+    }
+    else {
+        controlsEl.classList.toggle("d-none");
+        optionsEls.forEach(opEl => {
+            opEl.classList.remove("d-none", "active");
+            opEl.disabled = false;
+        })
+        const vsEl = document.querySelector(".vs-img");
+        vsEl.remove();
+        playAreaEl.classList.remove("d-none")
+        const cloneEl = document.querySelector(".clone-el");
+        if (cloneEl) cloneEl.remove();
+
+        const resultTextEl = document.querySelector("#resultText");
+        roundResultEl.removeChild(resultTextEl)
+    }
+
+}
+
+playBtnEl.addEventListener("click", showPlayOptions);
+
+optionsEls.forEach(el => {
+
+    el.addEventListener("click", function (e) {
+
+        if (e.target.nodeName == "IMG" || e.target.nodeName == "BUTTON") {
+
+            const computerSelection = computerPlay();
+            const playerSelection = e.target.dataset.value;
+            console.log("computer selection =>", computerSelection)
+            console.log("player selection =>", playerSelection)
+
+            const hideEls = gameValues.filter(item => item != computerSelection && item != playerSelection);
+
+            hideEls.forEach(value => {
+
+                optionsEls.forEach(opEl => {
+                    opEl.disabled = true;
+                    if (opEl.dataset.value == value) {
+                        opEl.classList.toggle("d-none")
+                    }
+                    if (opEl.dataset.value == playerSelection) {
+                        opEl.classList.add("active")
+                        playAreaEl.insertBefore(opEl, playAreaEl.firstChild);
+
+                    }
+                })
+
+            })
+
+            if (hideEls.length > 1) {
+                const cloneValue = gameValues.filter(item => item == computerSelection && item == playerSelection);
+                const elem = document.querySelector(`#${cloneValue}`);
+                const cloneEl = elem.cloneNode(true);
+                cloneEl.id = `${cloneValue}-2`;
+                cloneEl.classList.add("clone-el")
+                cloneEl.classList.remove("active")
+                elem.after(cloneEl);
+            }
+
+            const vsEl = document.createElement("img");
+            vsEl.src = "./assets/images/versus.png";
+            vsEl.alt = "VS";
+            vsEl.className = "vs-img";
+
+            const activeEl = document.querySelector(".active");
+            activeEl.after(vsEl);
+
+            controlsEl.classList.toggle("d-none")
+
+            const results = playRound(computerSelection, playerSelection);
+            console.log("Round result ==", results);
+
+            const resultTextEl = document.createElement("h3");
+            resultTextEl.id = "resultText";
+            resultTextEl.insertAdjacentText('afterbegin', results.displayText);
+            roundResultEl.appendChild(resultTextEl);
+
+            checkResults();
+        }
+
+    })
+
+});
+
+const playNextRoundEl = document.querySelector("#next-round");
+const viewResultsBtn = document.querySelector("#view-results");
+
+playNextRoundEl.addEventListener("click", () => {
+    roundCount++;
+    console.log("Round ", roundCount)
+    showPlayOptions();
+})
+
+const homeBtn = document.querySelector("#home-btn");
+const playAgainBtn = document.querySelector("#play-again");
+
+homeBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+playAgainBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+let getScoreText = () => {
+    if(gameResults.playerWinCount > gameResults.computerWinCount){
+        return "You Won!";
+    }
+    return "You Lost!";
+}
+
+
+let checkResults = () => {
+    if (gameResults.computerWinCount == 5 || gameResults.playerWinCount == 5) {
+        playNextRoundEl.classList.toggle("d-none");
+        viewResultsBtn.classList.toggle("d-none");
+
+        const scoreEl = document.querySelector(".score");
+        scoreEl.insertAdjacentText('afterbegin', `${gameResults.playerWinCount} - ${gameResults.computerWinCount}`);
+
+        const scoreTextEl = document.querySelector(".score-text");
+        scoreTextEl.insertAdjacentText('afterbegin', getScoreText())
+    }
+}
+
+
+viewResultsBtn.addEventListener("click", () => {
+    finalResultsEl.classList.toggle("d-none");
+    playAreaEl.classList.toggle("d-none");
+    controlsEl.classList.toggle("d-none");
+})
+
+
 let computerPlay = () => {
     let computerSelection = gameValues[Math.floor(Math.random() * gameValues.length)];
     return computerSelection;
 }
 
-let game = () => {
-    let playerWinCount = 0;
-    let computerWinCount = 0;
+let playRound = (computerSelection, playerSelection) => {
 
-    console.log("===== Welcome To The Game =====")
-
-    for (roundCount = 1; roundCount <= totalRounds; roundCount++) {
-        let results = playRound();
-        if (results) {
-            playerWinCount += results.playerWinCount;
-            computerWinCount += results.computerWinCount;
-
-            console.log("Your Score: ", playerWinCount);
-            console.log("Computer Score: ", computerWinCount);
-            console.log(`Results for round: ${roundCount} = `, results);
-        }
-        else {
-            roundCount--;
-        }
-
-    }
-
-    console.log(`==============================`);
-    if (playerWinCount > computerWinCount) {
-        console.log(`All Rounds Results: You Won!`);
-        console.log(`Your Score : ${playerWinCount}`);
-        console.log(`Opponent Score : ${computerWinCount}`);
-
-    }
-    else if (playerWinCount < computerWinCount) {
-        console.log(`All Rounds Results: You Lost!`);
-        console.log(`Your Score : ${playerWinCount}`);
-        console.log(`Opponent Score : ${computerWinCount}`);
-
-    }
-    else {
-        console.log(`All Rounds Results: Draw! `);
-        console.log(`Your Score : ${playerWinCount}`);
-        console.log(`Opponent Score : ${computerWinCount}`);
-    }
-
-}
-
-let playRound = () => {
-
-    let gameResults = {
-        playerWinCount: 0,
-        computerWinCount: 0,
-        displayText: ""
-    };
-
-    let playerSelection = prompt("Please enter your choice: Rock, Paper or Scissors.");
-    if (playerSelection) {
-
-        playerSelection = playerSelection.toLowerCase().trim();
-
-        if (playerSelection == gameValues[0] || playerSelection == gameValues[1] || playerSelection == gameValues[2]) {
-            console.log("user selection =>", playerSelection)
-
-            const computerSelection = computerPlay();
-            console.log("computer selection =>", computerSelection)
-
-            if (playerSelection != computerSelection) {
-                if (playerSelection == "rock" && computerSelection == "paper") {
-                    gameResults.computerWinCount = 1;
-                    gameResults.displayText = `You Lose! ${computerSelection} beats ${playerSelection}.`
-                    return gameResults;
-                }
-                else if (playerSelection == "rock" && computerSelection == "scissors") {
-                    gameResults.playerWinCount = 1;
-                    gameResults.displayText = `You Won! ${playerSelection} beats ${computerSelection}.`
-                    return gameResults;
-                }
-                else if (playerSelection == "paper" && computerSelection == "scissors") {
-                    gameResults.computerWinCount = 1;
-                    gameResults.displayText = `You Lose! ${computerSelection} beats ${playerSelection}.`
-                    return gameResults;
-                }
-                else if (playerSelection == "paper" && computerSelection == "rock") {
-                    gameResults.playerWinCount = 1;
-                    gameResults.displayText = `You Won! ${playerSelection} beats ${computerSelection}.`
-                    return gameResults;
-                }
-                else if (playerSelection == "scissors" && computerSelection == "rock") {
-                    gameResults.computerWinCount = 1;
-                    gameResults.displayText = `You Lose! ${computerSelection} beats ${playerSelection}.`
-                    return gameResults;
-                }
-                else if (playerSelection == "scissors" && computerSelection == "paper") {
-                    gameResults.playerWinCount = 1;
-                    gameResults.displayText = `You Won! ${playerSelection} beats ${computerSelection}.`
-                    return gameResults;
-                }
-            }
-            gameResults.displayText = `Draw!`;
+    if (playerSelection != computerSelection) {
+        if (playerSelection == "rock" && computerSelection == "paper") {
+            gameResults.computerWinCount++;
+            gameResults.displayText = `Oops! You Lose! ${computerSelection} beats ${playerSelection}.`
             return gameResults;
         }
-        else {
-            alert("Please enter correct option!");
-            return null;
+        else if (playerSelection == "rock" && computerSelection == "scissors") {
+            gameResults.playerWinCount++;
+            gameResults.displayText = `Yay! You Won! ${playerSelection} beats ${computerSelection}.`
+            return gameResults;
+        }
+        else if (playerSelection == "paper" && computerSelection == "scissors") {
+            gameResults.computerWinCount++;
+            gameResults.displayText = `Oops! You Lose! ${computerSelection} beats ${playerSelection}.`
+            return gameResults;
+        }
+        else if (playerSelection == "paper" && computerSelection == "rock") {
+            gameResults.playerWinCount++;
+            gameResults.displayText = `Yay! You Won! ${playerSelection} beats ${computerSelection}.`
+            return gameResults;
+        }
+        else if (playerSelection == "scissors" && computerSelection == "rock") {
+            gameResults.computerWinCount++;
+            gameResults.displayText = `Oops! You Lose! ${computerSelection} beats ${playerSelection}.`
+            return gameResults;
+        }
+        else if (playerSelection == "scissors" && computerSelection == "paper") {
+            gameResults.playerWinCount++;
+            gameResults.displayText = `Yay! You Won! ${playerSelection} beats ${computerSelection}.`
+            return gameResults;
         }
     }
-    else {
-        alert("Please enter correct option!");
-        return null;
-    }
-
+    gameResults.displayText = `Oops! It's a Draw!`;
+    return gameResults;
 }
-
-game();
-
 
